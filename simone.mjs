78bbbee5694567ee2a0d782ad5aff0527eb30af4
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-12-12 13:20:18 +0100

    fix(lefthook): apply fix on commit, update git index (#4)
    * fix(lefthook): fix on commit, update git index
    
    * fix(ci): use pnpm for build
    
    * chore: move dmg-license to dev-deps
    
    * chore: split check and release

--- simone.mjs
+++ simone.mjs
@@ -17,60 +17,60 @@ let timer;
 const interval = 1000 * 60; // 60 secondi
 
 function createOverlay() {
-  overlayWindow = new BrowserWindow({
-    width: 230,
-    height: 250,
-    transparent: true,
-    frame: false,
-    alwaysOnTop: true,
-    x: 200,
-    y: 200,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
+	overlayWindow = new BrowserWindow({
+		width: 230,
+		height: 250,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true,
+		x: 200,
+		y: 200,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
 
-  overlayWindow.loadFile(path.join(process.cwd(), "overlay.html"));
-  overlayWindow.setResizable(false);
+	overlayWindow.loadFile(path.join(process.cwd(), "overlay.html"));
+	overlayWindow.setResizable(false);
 }
 
 function moveMouseRandomly() {
-  const screenSize = robot.getScreenSize();
-  const x = Math.floor(Math.random() * screenSize.width);
-  const y = Math.floor(Math.random() * screenSize.height);
+	const screenSize = robot.getScreenSize();
+	const x = Math.floor(Math.random() * screenSize.width);
+	const y = Math.floor(Math.random() * screenSize.height);
 
-  robot.moveMouse(x, y);
-  console.log(`Mouse automatically moved to: (${x}, ${y})`);
+	robot.moveMouse(x, y);
+	console.log(`Mouse automatically moved to: (${x}, ${y})`);
 }
 
 function checkMouseActivity() {
-  const currentMousePosition = robot.getMousePos();
+	const currentMousePosition = robot.getMousePos();
 
-  if (
-    currentMousePosition.x !== lastMousePosition.x ||
-    currentMousePosition.y !== lastMousePosition.y
-  ) {
-    console.log("Mouse movement detected! Timer reset.");
-    lastMousePosition = currentMousePosition;
+	if (
+		currentMousePosition.x !== lastMousePosition.x ||
+		currentMousePosition.y !== lastMousePosition.y
+	) {
+		console.log("Mouse movement detected! Timer reset.");
+		lastMousePosition = currentMousePosition;
 
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(moveMouseRandomly, interval);
-  }
+		if (timer) clearTimeout(timer);
+		timer = setTimeout(moveMouseRandomly, interval);
+	}
 }
 
 app.on("ready", () => {
-  console.log("Simone.js Mouse Mover v.1.1");
-  console.log(
-    `Script started! Timer set to ${
-      interval / 1000
-    } seconds. Press CTRL+C to stop.`
-  );
-  createOverlay();
+	console.log("Simone.js Mouse Mover v.1.1");
+	console.log(
+		`Script started! Timer set to ${
+			interval / 1000
+		} seconds. Press CTRL+C to stop.`,
+	);
+	createOverlay();
 
-  setInterval(checkMouseActivity, 1000);
+	setInterval(checkMouseActivity, 1000);
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
